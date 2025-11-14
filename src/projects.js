ac8c796b61df94a82d65209bb225691e1f8397e8
ProudMay <mehdi79nekouei@gmail.com>
2025-11-14 10:36:43 +0330

    feat [ todo remover function ]

--- src/projects.js
+++ src/projects.js
@@ -7,4 +7,10 @@ function projCreator(title){
     function addTodo(todo){
         todos.push(todo);
     }
+
+    function removeTodo (todoId){
+        const index = todos.findIndex(t => t.id === todoId);
+        if(index !== -1) todos.splice(index,1);
+    }
+    
 }
