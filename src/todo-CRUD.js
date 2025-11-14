26bd2dbd0dac0263373b0c87fc8fc0264299c7e2
ProudMay <mehdi79nekouei@gmail.com>
2025-11-14 11:37:28 +0330

    feat [ C create func  of CRUD ]

--- src/todo-CRUD.js
+++ src/todo-CRUD.js
@@ -6,3 +6,16 @@
 // D : Delete
 
 import {createTodo } from './todo-factory.js'
+
+const todos = [];
+
+const todoController = {
+    //C => Create :
+
+    create({title, descriptions, date, note, isDone=false}){
+        const todo = createTodo(title, descriptions, date, note, isDone);
+        todos.push(todo);
+        return todo;
+    }
+
+}
