efa21fd70c785b7525a6b249cf8e0769d0e660f7
ProudMay <mehdi79nekouei@gmail.com>
2025-11-14 13:39:20 +0330

    My brain  Off  / Chat GPT on

--- src/index.js
+++ src/index.js
@@ -1,68 +1,102 @@
 import './styles.css';
 import { todoController } from './todo-CRUD.js';
-import {projectCreator} from './projects.js';
-import { addTodo } from './todo-factory.js';
+import { projectCreator } from './projects.js';
 import { initDOM } from './dom.js';
 
-
-
 let projects = [];
-let todos = [];
 let currentProject = null;
 
-function createDefaultProject () {
-    const defaultProject = projectCreator('Inbox');
-    projects.push(defaultProject);
-    currentProject = defaultProject;
+function createDefaultProject() {
+  const defaultProject = projectCreator('Inbox');
+  projects.push(defaultProject);
+  currentProject = defaultProject;
 }
 
-function createNewProject (title){
-    const project = projectCreator(title);
-    projects.push(project);
-    return project;
+function createNewProject(title) {
+  const project = projectCreator(title);
+  projects.push(project);
+  currentProject = project;
+  return project;
 }
 
-function addTodoToActiveProject({title,descriptions,date,note,isDone=false}){
-    if(!currentProject){
-        console.error("There is no Project !");
-        return;
-    }
-    const todo = todoController.create({title, descriptions, date,note,isDone});
-    currentProject.addTodo(todo);
-    
-    return todo;
+function addTodoToActiveProject({ title, descriptions, date, note, isDone = false }) {
+  if (!currentProject) {
+    console.error('There is no Project !');
+    return;
+  }
+  const todo = todoController.create({ title, descriptions, date, note, isDone });
+  currentProject.addTodo(todo);
+  return todo;
 }
 
-function getActiveProjectTodos (){
-    if(!currentProject) return [];
-    return currentProject.getTodos();
+function getActiveProjectTodos() {
+  if (!currentProject) return [];
+  return currentProject.getTodos();
 }
 
-function getAllProjects(){
-    return[...projects];
+function getAllProjects() {
+  return [...projects];
 }
 
-
-function init (){
-    createDefaultProject();
-
-    addTodoToActiveProject({
-        title : " Go Fuck YourSelf",
-        descriptions :"because im angry right now !",
-        date :"every day",
-        note :"just kidding"
-    });
-    console.log('All Projects :',projects);
-    console.log('Todos :',getActiveProjectTodos());
-
+function getActiveProject() {
+  return currentProject;
 }
-function setActiveProject(projectId){
-    const project = projects.find(p => p.id === projectId);
-    if(!project){
-        console.error("Project not found!");
-        return;
-    }
-    currentProject = project;
+
+function setActiveProject(projectId) {
+  const project = projects.find((p) => p.id === projectId);
+  if (!project) {
+    console.error('Project not found!');
+    return;
+  }
+  currentProject = project;
+}
+
+function deleteTodoFromActiveProject(todoId) {
+  if (!currentProject) return;
+  currentProject.removeTodo(todoId);
+}
+
+function toggleTodoDone(todoId) {
+  const todos = getActiveProjectTodos();
+  const todo = todos.find((t) => t.id === todoId);
+  if (todo && typeof todo.toggleDone === 'function') {
+    todo.toggleDone();
+  }
+}
+
+function init() {
+  createDefaultProject();
+
+  addTodoToActiveProject({
+    title: 'Go Fuck YourSelf',
+    descriptions: 'because im angry right now !',
+    date: 'every day',
+    note: 'just kidding',
+  });
+
+  console.log('All Projects :', projects);
+  console.log('Todos :', getActiveProjectTodos());
+
+  initDOM({
+    getProjects: getAllProjects,
+    getActiveProject,
+    getActiveProjectTodos,
+    onAddProject: (title) => {
+      createNewProject(title);
+    },
+    onChangeProject: (projectId) => {
+      setActiveProject(projectId);
+    },
+    onAddTodo: (todoData) => {
+      addTodoToActiveProject(todoData);
+    },
+    onToggleTodo: (todoId) => {
+      toggleTodoDone(todoId);
+    },
+    onDeleteTodo: (todoId) => {
+      deleteTodoFromActiveProject(todoId);
+    },
+  });
 }
 
 init();
@@ -70,6 +104,7 @@ init();
 window.app = {
   projects,
   getAllProjects,
+  getActiveProject,
   getActiveProjectTodos,
   createNewProject,
   setActiveProject,
